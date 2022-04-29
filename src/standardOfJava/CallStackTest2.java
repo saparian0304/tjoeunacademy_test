4217319ea6adc7d18c6d030a81f937e5505eab1d
saparian0304 <anjp0304@gmail.com>
2022-04-29 09:04:08 +0900

    자바_객체지향프로그래밍

---
+++ src/standardOfJava/CallStackTest2.java
@@ -0,0 +1,22 @@
+package standardOfJava;
+
+public class CallStackTest2 {
+
+	public static void main(String[] args) {
+		System.out.println("main(String[] args)가 시작되었음");
+		firstMethod();
+		System.out.println("main(String[] args)가 끝났음");
+
+	}
+	
+	static void firstMethod() {
+		System.out.println("firsrMethod()가 시작되었음");
+		secondMethod();
+		System.out.println("firstMethod()가 끝났음");
+	}
+	static void secondMethod() {
+		System.out.println("secondMethod()가 시작되었음");
+		System.out.println("secondMethod()가 끝났음");
+	}
+
+}
---
+++ src/standardOfJava/ReferenceParamEx.java
@@ -0,0 +1,21 @@
+package standardOfJava;
+
+class Data { int x; }
+public class ReferenceParamEx {
+
+	public static void main(String[] args) {
+		Data d = new Data();
+		d.x = 10;
+		System.out.println("Main() : x = " + d.x);
+		
+		change(d);
+		System.out.println("After change(d)");
+		System.out.println("Main() : x = " + d.x);
+	}
+	
+	static void change(Data d) {		// 참조형 매개변수
+		d.x = 1000;
+		System.out.println("change() : x = " + d.x);
+	}
+
+}
