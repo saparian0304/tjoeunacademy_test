e2d84e8e6f6f463e907b50cfe352e8272516ca63
saparian0304 <anjp0304@@gmail.com>
2022-05-11 09:07:18 +0900

    선행학습 자료 정리_"past_class.zip"

--- src/chapter02/HelloJava.java
+++ src/chapter02/HelloJava.java
@@ -1,23 +1,32 @@
 package chapter02;
 
 public class HelloJava {
+
 	public static void main(String[] args) {
-		//System.out.println("안녕하세요");
-		/*
-		 * 여기는 여러줄 주석
-		 * 전부 실행이 안됨 
-		 */
-		// 출력문 단축키 sysout ctrl+space
-		// 주석 단축키 ctrl + '/'
-		System.out.println("hello");
-		System.out.println("hello");
-		System.out.println("hello");
-		System.out.println("hello");
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java             ");
+		// 안녕하세요
+//		안녕하세요
 		
 		/*
-		 * hello가 출력되게 하기 위해서
-		 * 2022.04.25 anjp0304
+		 * adfae
+		 * dafe
+		 * sdege
+		 * daefa
 		 */
-		System.out.println("hello");
+		
+		/*
+		 기획팀 누가 A라는 이유로 B하라고 해서 ~함
+		 By ooooo 2022.05.10
+		 */
+		
+		System.out.println("Hello Java             ");
+		System.out.println("Hello Java             ");
+		System.out.println("Hello Java             ");
+		System.out.println("Hello Java             ");
+		System.out.println("Hello Java             ");
+		System.out.println("Hello Java             ");
+		System.out.println("Hello Java             ");
 	}
+
 }
--- src/cloud/KimBob.java
+++
@@ -1,8 +0,0 @@
-package cloud;
-
-public class KimBob {
-	public static void main(String[] args) {
-		System.out.println(args[0] + " 김밥~");
-		System.out.println(args[1] + " 김밥~");
-	}
-}
